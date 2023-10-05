fad5850b1a6f307b78fe57831558ca53db73a7b5
Impossible777 <cristmichael7@gmail.com>
2023-10-05 15:55:25 -0500

    Created function to simplify creating workouts

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import loadPage from "../workout";
+import './style.css'
 
 loadPage();
--- workout.js
+++ workout.js
@@ -11,6 +11,23 @@ const legRaisesElement = document.createElement('div');
 legRaisesElement.classList.add('workout');
 const inputElement = document.createElement('input');
 
+//Exerise Details
+const numberOfSetsElement = document.createElement('div');
+numberOfSetsElement.classList.add('Sets');
+numberOfSetsElement.textContent = 'Sets';
+const RPEElement = document.createElement('div');
+RPEElement.classList.add('RPE');
+RPEElement.textContent = 'RPE';
+const REPElement = document.createElement('div');
+REPElement.classList.add('REP');
+REPElement.textContent = 'REP';
+
+//
+
+
+
+
+
 function loadPage() {
 
 content.appendChild(squatElement);
@@ -23,8 +40,73 @@ RDLElement.textContent = 'Romanian Deadlift';
 legExtensionElement.textContent = 'Leg Extension';
 seatedCalfRaiseElement.textContent = 'Seated Calf Raise';
 legRaisesElement.textContent = 'Leg Raises';
+addExerciseDetails(squatElement);
+addExerciseDetails(RDLElement);
+addExerciseDetails(legExtensionElement);
+addExerciseDetails(seatedCalfRaiseElement);
+addExerciseDetails(legRaisesElement);
+
+var editableTextElements = document.querySelectorAll('.editableText');
+editableTextElements.forEach(function(editableText){
+
+
+editableText.addEventListener("click", function() {
+editableText.contentEditable = true;
+editableText.focus();
+
+editableText.addEventListener("blur", function(){
+editableText.contentEditable = false;
+})
+
+})
+});
+
+var elementsWithClassEditableText = document.getElementsByClassName('editableText');
+elementsWithClassEditableText[0].textContent = '3-5';
+elementsWithClassEditableText[1].textContent = '4-6';
+elementsWithClassEditableText[2].textContent = '6-8';
+elementsWithClassEditableText[3].textContent = '4-6';
+elementsWithClassEditableText[4].textContent = '6-8';
+
+
 
 
 }
+
+function addExerciseDetails(workout) {
+    const numberOfSetsElement = document.createElement('div');
+    numberOfSetsElement.classList.add('Sets');
+    numberOfSetsElement.textContent = 'Sets';
+    const RPEElement = document.createElement('div');
+    RPEElement.classList.add('RPE');
+    RPEElement.textContent = 'RPE';
+    const REPElement = document.createElement('div');
+    REPElement.classList.add('REP');
+    REPElement.textContent = 'REP';
+    const editableTextDiv = document.createElement('div');
+    editableTextDiv.classList = 'editableText';
+    editableTextDiv.contentEditable = 'true';
+    editableTextDiv.textContent = "REPS";
+    REPElement.appendChild(editableTextDiv);
+
+
+
+
+
+
+    workout.appendChild(numberOfSetsElement);
+    workout.appendChild(RPEElement);
+    workout.appendChild(REPElement);
+
+
+
+
+}
+
+
+
+
+    
+
 
 export default loadPage;
